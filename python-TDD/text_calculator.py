057640486e952c86589bca6709b6a840c70e1942
Caio Dantas <caiodantasl.lara@gmail.com>
2023-11-10 16:33:31 -0300

    create more tests

--- python-TDD/text_calculator.py
+++ python-TDD/text_calculator.py
@@ -6,4 +6,27 @@ class TestCalculator(unittest.TestCase):
     def test_sum_5_and_5_return_10(self):
         self.assertEqual(sum(5, 5), 10)
 
-    unittest.main()
+    def test_sum_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 20),
+            (5, 10, 15),
+            (-2, 10, 8),
+            (10.5, 10.5, 21),
+            (10.5, -10, 0.5),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(sum(x, y), saida)
+
+    def test_sum_x__nao_e_int_ou_float_deve_retornar_assertionError(self):
+        with self.assertRaises(AssertionError):
+            sum('11', 0)
+
+    def test_sum_y__nao_e_int_ou_float_deve_retornar_assertionError(self):
+        with self.assertRaises(AssertionError):
+            sum('11', 0)
+
+
+unittest.main(verbosity=2)
