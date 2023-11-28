e33d3a117f33af37b042d760b665f367f7c630a9
Caio Dantas <caiodantasl.lara@gmail.com>
2023-11-28 15:25:22 -0300

    tdd and unittest

--- python-TDD/baconcomovos.py
+++ python-TDD/baconcomovos.py
@@ -1,3 +1,29 @@
 """
 
 """
+"""
+1 - Receber um número inteiro 
+2 - Saber se o número é multiplo de 3 e 5:
+    Bacon com ovos
+3 - Saber se o número é multipliO somente de 3:
+    Bacon
+4 - Saber se o número é multiplo somente de 5:
+    Ovos
+5 - Saber se o número NÃO é multiplo de 3 e 5:
+    Passa fome
+"""
+
+
+def bacon_com_ovos(n):
+    assert isinstance(n, int), 'n not INT'
+
+    if n % 3 == 0 and n % 5 == 0:
+        return 'Bacon com ovos'
+
+    if n % 3 == 0:
+        return 'Bacon'
+
+    if n % 5 == 0:
+        return 'Ovos'
+
+    return 'Passar fome'
